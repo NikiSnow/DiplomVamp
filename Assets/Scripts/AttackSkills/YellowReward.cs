bda442e37a42723f4eb765be657e0d0a9ca0db39
NikiSnow <snow.12356@ya.ru>
2026-05-06 08:59:03 +0300

    FixtheMerge?

---
+++ Assets/Scripts/AttackSkills/YellowReward.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class YellowReward : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    [SerializeField] Player ThePlayer;
+}
---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    [SerializeField] Casino CasinoScr;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            CasinoScr.StartGambling();
+        }
+    }
+}
